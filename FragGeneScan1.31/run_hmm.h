a21554ff5bf18566e7cfc0f3b74a8e2883f1dd75
Tibo Vanheule <tibo@tibovanheule.space>
2020-06-06 00:20:29 +0200

    small cleanup

---
+++ FragGeneScan1.31/run_hmm.h
@@ -0,0 +1,34 @@
+#ifndef RUN_HMM_H
+#define RUN_HMM_H
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include "hmm.h"
+
+#include <pthread.h>
+
+typedef struct thread_data {
+	FILE *out;
+	FILE *aa;
+	FILE *dna;
+	char *obs_head;
+	char *obs_seq;
+	int wholegenome;
+	int cg;
+	int format;
+	HMM *hmm;
+	TRAIN *train;
+} thread_data;
+
+void* thread_func(void *threadarr);
+
+void print_error(const char* error_message, ...);
+
+void print_file_error(const char* error_message, char* file);
+
+#endif
