1220c6dcc19d8b93c14abaadf08cd55b7fb79fae
Tibo Vanheule <tibo@tibovanheule.space>
2020-06-06 00:18:07 +0200

    mac install scripts

---
+++ install scripts/mac_doxygen.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# install Graphviz2
+brew install graphviz
+# install doxygen
+brew install doxygen
