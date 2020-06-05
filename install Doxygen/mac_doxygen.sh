854f7c859fc0b0a09e8764c8b370278255526e76
Tibo Vanheule <tibo@tibovanheule.space>
2020-06-06 00:19:29 +0200

    unnessercary files

--- install Doxygen/mac_doxygen.sh
+++
@@ -1,6 +0,0 @@
-# install homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-# install Graphviz2
-brew install graphviz
-# install doxygen
-brew install doxygen
