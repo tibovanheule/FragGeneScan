9b7de4d530308407202b5106e16423670050cbce
Tibo Vanheule <tibo@tibovanheule.space>
2020-06-06 10:15:59 +0200

    rename source folder

--- push.sh
+++ push.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+
 # git pusher will test the program first and generate documentation
+
+# generate doxygen
 doxygen "./doxygen/Doxyfile mac"
+# add new doc files to a commmit
 git add "./FragGeneScan1.31/doc/" && git commit -m"new doc (auto generated)"
+# navigate to latex doc folder
+cd ./FragGeneScan1.31/doc/latex/
+# generate new pdf
+pdflatex -halt-on-error -output-directory=../../../ -jobname="Fraggenescan documentation" refman.tex
+cd ../../../
+# commit new pdf
+git add "Fraggenescan documentation.pdf" && git commit -m"new doc pdf (auto generated)"
+# push all
 git push origin master
