a6867775f89112bf898af2f27dd1289f0d465603
Tibo Vanheule <tibo@tibovanheule.space>
2020-06-10 22:17:04 +0200

    git pull first

--- push.sh
+++ push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # git pusher will test the program first and generate documentation
-
+git pull
 # generate doxygen
 doxygen "./doxygen/Doxyfile mac"
 # add new doc files to a commmit
