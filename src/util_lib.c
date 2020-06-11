dfac5a9944c862f9ba72d24c9b2277f6c0f1720e
Tibo Vanheule <tibo@tibovanheule.space>
2020-06-12 00:18:19 +0200

    formatted code

--- src/util_lib.c
+++ src/util_lib.c
@@ -5,61 +5,63 @@
 #include "util_lib.h"
 #define TR_SIZE 14
 
- char* tr_list[TR_SIZE] = { "MM","MI","MD","II","IM","DD","DM","GE","GG","ER","RS","RR","ES","ES1" };
+char* tr_list[TR_SIZE] = { "MM","MI","MD","II","IM","DD","DM","GE","GG","ER","RS","RR","ES","ES1" };
 
- char codon5[5] = { 'A', 'C', 'G', 'T', 'N' };
- char codon11[11] = { 'A', 'C', 'G', 'T', 'N', 'a', 'c', 'g', 't', 'n', 'x' };
+char codon5[5] = { 'A', 'C', 'G', 'T', 'N' };
+char codon11[11] = { 'A', 'C', 'G', 'T', 'N', 'a', 'c', 'g', 't', 'n', 'x' };
 
- char codon_code[65] = { 'K','N','K','N',
-             'T','T','T','T',
-             'R','S','R','S',
-             'I','I','M','I',
-             'Q','H','Q','H',
-             'P','P','P','P',
-             'R','R','R','R',
-             'L','L','L','L',
-             'E','D','E','D',
-             'A','A','A','A',
-             'G','G','G','G',
-             'V','V','V','V',
-             '*','Y','*','Y',
-             'S','S','S','S',
-             '*','C','W','C',
-             'L','F','L','F', 'X' };
+char codon_code[65] = { 'K','N','K','N',
+                        'T','T','T','T',
+                        'R','S','R','S',
+                        'I','I','M','I',
+                        'Q','H','Q','H',
+                        'P','P','P','P',
+                        'R','R','R','R',
+                        'L','L','L','L',
+                        'E','D','E','D',
+                        'A','A','A','A',
+                        'G','G','G','G',
+                        'V','V','V','V',
+                        '*','Y','*','Y',
+                        'S','S','S','S',
+                        '*','C','W','C',
+                        'L','F','L','F', 'X'
+                      };
 
 char anti_codon_code[65] = { 'F','V','L','I',
-             'C','G','R','S',
-             'S','A','P','T',
-             'Y','D','H','N',
-             'L','V','L','M',
-             'W','G','R','R',
-             'S','A','P','T',
-             '*','E','Q','K',
-             'F','V','L','I',
-             'C','G','R','S',
-             'S','A','P','T',
-             'Y','D','H','N',
-             'L','V','L','I',
-             '*','G','R','R',
-             'S','A','P','T',
-             '*','E','Q','K','X' };
+                             'C','G','R','S',
+                             'S','A','P','T',
+                             'Y','D','H','N',
+                             'L','V','L','M',
+                             'W','G','R','R',
+                             'S','A','P','T',
+                             '*','E','Q','K',
+                             'F','V','L','I',
+                             'C','G','R','S',
+                             'S','A','P','T',
+                             'Y','D','H','N',
+                             'L','V','L','I',
+                             '*','G','R','R',
+                             'S','A','P','T',
+                             '*','E','Q','K','X'
+                           };
 
 /**
 * Makes an matrix with datatype double.
 * Elements are double pointers en matrix is a double double pointer (**pointer).
 * Exits when allocation fails.
 */
-double **dmatrix(int num_row, int num_col){
+double **dmatrix(int num_row, int num_col) {
 
-  double **m = (double **) malloc(num_row * sizeof(double*));
+    double **m = (double **) malloc(num_row * sizeof(double*));
 
-  if (!m) print_allocation_error("%s\n", "ERROR: Allocation failure for points to rows in dmatrix()");
+    if (!m) print_allocation_error("%s\n", "ERROR: Allocation failure for points to rows in dmatrix()");
 
-  for(int i=0; i<num_row; i++) {
-    m[i]=(double *) calloc(num_col, sizeof(double));
-    if (!m[i]) print_allocation_error("%s %d %s\n", "ERROR: Allocation failure for the row ", i, " in dmatrix()");
-  }
-  return m;
+    for(int i=0; i<num_row; i++) {
+        m[i]=(double *) calloc(num_col, sizeof(double));
+        if (!m[i]) print_allocation_error("%s %d %s\n", "ERROR: Allocation failure for the row ", i, " in dmatrix()");
+    }
+    return m;
 }
 
 /**
@@ -67,18 +69,18 @@ double **dmatrix(int num_row, int num_col){
 * Elements are int pointers en matrix is a double int pointer.
 * Exits when allocation fails.
 */
-int **imatrix(int num_row, int num_col){
+int **imatrix(int num_row, int num_col) {
 
-  int i,j;
-  int **m = (int **) malloc(num_row * sizeof(int*));
+    int i,j;
+    int **m = (int **) malloc(num_row * sizeof(int*));
 
-  if (!m) print_allocation_error("%s\n", "ERROR: Allocation failure for points to rows in imatrix()");
+    if (!m) print_allocation_error("%s\n", "ERROR: Allocation failure for points to rows in imatrix()");
 
-  for(i=0; i<num_row; i++) {
-    m[i]=(int *) calloc(num_col, sizeof(int));
-    if (!m[i]) print_allocation_error("%s %d %s\n", "ERROR: Allocation failure for the row ", i ," in imatrix()");
-  }
-  return m;
+    for(i=0; i<num_row; i++) {
+        m[i]=(int *) calloc(num_col, sizeof(int));
+        if (!m[i]) print_allocation_error("%s %d %s\n", "ERROR: Allocation failure for the row ", i," in imatrix()");
+    }
+    return m;
 }
 
 /**
@@ -86,16 +88,16 @@ int **imatrix(int num_row, int num_col){
 * Elements are doubles en vector is a double pointer.
 * Exits when allocation fails.
 */
-double *dvector(int nh){
+double *dvector(int nh) {
 
-  int j;
-  double *v = (double *) malloc(nh * sizeof(double));
+    int j;
+    double *v = (double *) malloc(nh * sizeof(double));
 
-  if (!v) print_allocation_error("%s\n", "ERROR: Allocation failure in dvector()");
+    if (!v) print_allocation_error("%s\n", "ERROR: Allocation failure in dvector()");
 
-  for(j=0; j<nh; j++) v[j] = 0.0;
+    for(j=0; j<nh; j++) v[j] = 0.0;
 
-  return v;
+    return v;
 }
 
 /**
@@ -103,58 +105,58 @@ double *dvector(int nh){
 * Elements are ints en vector is a int pointer.
 * Exits when allocation fails.
 */
-int *ivector(int nh){
-  int *v=(int *)malloc(nh * sizeof(int));
+int *ivector(int nh) {
+    int *v=(int *)malloc(nh * sizeof(int));
 
-  if (!v) print_allocation_error("%s\n", "ERROR: Allocation failure in ivector()");
+    if (!v) print_allocation_error("%s\n", "ERROR: Allocation failure in ivector()");
 
-  for(int j=0; j<nh; j++) v[j] = 0;
-  return v;
+    for(int j=0; j<nh; j++) v[j] = 0;
+    return v;
 }
 
 /**
 * Frees the memory allocation of an vector with datatype double.
 */
-void free_dvector(double *v){
-  free(v);
+void free_dvector(double *v) {
+    free(v);
 }
 
 /**
 * Frees the memory allocation of an vector with datatype int.
 */
-void free_ivector(int *v){
-  free(v);
+void free_ivector(int *v) {
+    free(v);
 }
 
 /**
 * Frees the memory allocation of an matrix with datatype double.
 */
-void free_dmatrix(double **m, int num_row){
-  for(int i=num_row-1; i>=0; i--) free(m[i]);
-  free(m);
+void free_dmatrix(double **m, int num_row) {
+    for(int i=num_row-1; i>=0; i--) free(m[i]);
+    free(m);
 }
 
 /**
 * Frees the memory allocation of an matrix with datatype int.
 */
-void free_imatrix(int **m,int num_row){
-  for(int i=num_row-1; i>=0; i--)    free(m[i]);
-  free(m);
+void free_imatrix(int **m,int num_row) {
+    for(int i=num_row-1; i>=0; i--)    free(m[i]);
+    free(m);
 }
 
 /**
 * Converts a given transition to int. Use for example as indexing.
 * switch case not possible due the fact that strings are not constonant.
 */
-int tr2int(char *tr){
-  size_t index=0;
-  while(index < TR_SIZE && tr_list[index] != tr) ++index;
-  return index;
+int tr2int(char *tr) {
+    size_t index=0;
+    while(index < TR_SIZE && tr_list[index] != tr) ++index;
+    return index;
 }
 
 
-int nt2int (char nt){
-  switch(nt){
+int nt2int (char nt) {
+    switch(nt) {
     case 'A':
     case 'a':
         return 0;
@@ -169,12 +171,12 @@ int nt2int (char nt){
         return 3;
     default:
         return 4;
-  }
+    }
 }
 
 
-int nt2int_rc (char nt){
-  switch(nt){
+int nt2int_rc (char nt) {
+    switch(nt) {
     case 'A':
     case 'a':
         return 3;
@@ -189,11 +191,11 @@ int nt2int_rc (char nt){
         return 0;
     default:
         return 4;
-  }
+    }
 }
 
-int nt2int_rc_indel (char nt){
-  switch(nt){
+int nt2int_rc_indel (char nt) {
+    switch(nt) {
     case 'A':
         return 3;
     case 'C':
@@ -216,15 +218,15 @@ int nt2int_rc_indel (char nt){
         return 10;
     default:
         return 4;
-  }
+    }
 }
 
 
-int trinucleotide (char a, char b, char c){
+int trinucleotide (char a, char b, char c) {
 
-  int freq_id;
+    int freq_id;
 
-  switch(a){
+    switch(a) {
     case 'A':
     case 'a':
         freq_id = 0;
@@ -244,9 +246,9 @@ int trinucleotide (char a, char b, char c){
     default:
         freq_id = 0;
         break;
-  }
+    }
 
-  switch(b){
+    switch(b) {
     case 'A':
     case 'a':
         break;
@@ -264,9 +266,9 @@ int trinucleotide (char a, char b, char c){
         break;
     default:
         break;
-  }
+    }
 
-  switch(c){
+    switch(c) {
     case 'A':
     case 'a':
         return freq_id;
@@ -284,14 +286,14 @@ int trinucleotide (char a, char b, char c){
         return freq_id;
     default:
         return freq_id;
-  }
+    }
 }
 
-int trinucleotide_pep (char a, char b, char c){
+int trinucleotide_pep (char a, char b, char c) {
 
-  int freq_id;
+    int freq_id;
 
-  switch(a){
+    switch(a) {
     case 'A':
     case 'a':
         freq_id = 0;
@@ -311,10 +313,10 @@ int trinucleotide_pep (char a, char b, char c){
     default:
         freq_id = 64;
         break;
-  }
+    }
 
-  if (freq_id < 64){
-      switch(b){
+    if (freq_id < 64) {
+        switch(b) {
         case 'A':
         case 'a':
             break;
@@ -333,11 +335,11 @@ int trinucleotide_pep (char a, char b, char c){
         default:
             freq_id = 64;
             break;
-      }
-  }
+        }
+    }
 
-  if (freq_id < 64){
-      switch(c){
+    if (freq_id < 64) {
+        switch(c) {
         case 'A':
         case 'a':
             return freq_id;
@@ -355,95 +357,95 @@ int trinucleotide_pep (char a, char b, char c){
             return freq_id;
         default:
             return 64;
-      }
-  }
-  return freq_id;
+        }
+    }
+    return freq_id;
 }
 
 /**
-* copies dna to dna1 in reverse. and 
+* copies dna to dna1 in reverse. and
 */
-void get_rc_dna(char *dna, char *dna1){
-  int dna_len = strlen(dna);
-  for (int i=0; i<dna_len; i++) dna1[dna_len-i-1] = codon5[nt2int_rc(dna[i])];
+void get_rc_dna(char *dna, char *dna1) {
+    int dna_len = strlen(dna);
+    for (int i=0; i<dna_len; i++) dna1[dna_len-i-1] = codon5[nt2int_rc(dna[i])];
 }
 
 /**
-* copies dna to dna1 in reverse. and 
+* copies dna to dna1 in reverse. and
 */
-void get_rc_dna_indel(char *dna, char *dna1){
-  int dna_len = strlen(dna);
-  for ( int i=0; i<dna_len; i++) dna1[dna_len-i-1] = codon11[nt2int_rc_indel(dna[i])];
+void get_rc_dna_indel(char *dna, char *dna1) {
+    int dna_len = strlen(dna);
+    for ( int i=0; i<dna_len; i++) dna1[dna_len-i-1] = codon11[nt2int_rc_indel(dna[i])];
 }
 
 /**
 * Get a protein of dna
 * if Whole_genome equals to zero, then we want a short read and stop early.
 */
-void get_protein(char *dna, char *protein,  int strand, int whole_genome){
+void get_protein(char *dna, char *protein,  int strand, int whole_genome) {
 
-  int dna_len = strlen(dna);
+    int dna_len = strlen(dna);
 
-  if (strand == 1){
-    for (int i=0; i<dna_len; i+=3) protein[i/3] = codon_code[trinucleotide_pep(dna[i], dna[i+1], dna[i+2])];
-  }else{
-    int protein_len = dna_len/3;
+    if (strand == 1) {
+        for (int i=0; i<dna_len; i+=3) protein[i/3] = codon_code[trinucleotide_pep(dna[i], dna[i+1], dna[i+2])];
+    } else {
+        int protein_len = dna_len/3;
 
-    for (int i=0; i<dna_len; i+=3){
-      protein[(dna_len-i)/3-1] = anti_codon_code[trinucleotide_pep(dna[i], dna[i+1], dna[i+2])];
-      protein_len--;
+        for (int i=0; i<dna_len; i+=3) {
+            protein[(dna_len-i)/3-1] = anti_codon_code[trinucleotide_pep(dna[i], dna[i+1], dna[i+2])];
+            protein_len--;
+        }
     }
-  }
 
 //remove the ending *
-  if(protein[strlen(protein) - 1] == '*') protein[strlen(protein) - 1] = 0;
-  
-  //alternative start codons still encode for Met
-  //E. coli uses 83% AUG (3542/4284), 14% (612) GUG, 3% (103) UUG and one or two others (e.g., an AUU and possibly a CUG)
-  //only consider two major alternative ones, GTG and TTG f
+    if(protein[strlen(protein) - 1] == '*') protein[strlen(protein) - 1] = 0;
 
-  if(whole_genome == 0) return; //short reads, skip
+    //alternative start codons still encode for Met
+    //E. coli uses 83% AUG (3542/4284), 14% (612) GUG, 3% (103) UUG and one or two others (e.g., an AUU and possibly a CUG)
+    //only consider two major alternative ones, GTG and TTG f
 
-  if(strand == 1) {
-  	int s = trinucleotide_pep(dna[0], dna[1], dna[2]);
-	if(s == trinucleotide_pep('G', 'T', 'G') || s == trinucleotide_pep('T', 'T', 'G')) protein[0] = 'M';
-  } else {
-  	int s = trinucleotide_pep(dna[dna_len - 3], dna[dna_len - 2], dna[dna_len - 1]);
-	if(s == trinucleotide_pep('C', 'A', 'C') || s == trinucleotide_pep('C', 'A', 'A')) protein[0] = 'M';
-  }
-} 
+    if(whole_genome == 0) return; //short reads, skip
+
+    if(strand == 1) {
+        int s = trinucleotide_pep(dna[0], dna[1], dna[2]);
+        if(s == trinucleotide_pep('G', 'T', 'G') || s == trinucleotide_pep('T', 'T', 'G')) protein[0] = 'M';
+    } else {
+        int s = trinucleotide_pep(dna[dna_len - 3], dna[dna_len - 2], dna[dna_len - 1]);
+        if(s == trinucleotide_pep('C', 'A', 'C') || s == trinucleotide_pep('C', 'A', 'A')) protein[0] = 'M';
+    }
+}
 
 /**
 * Print how the program should be used.
 * called mainly on help or error.
 */
-void print_usage(){
+void print_usage() {
 
-  printf("%s", "USAGE: ./FragGeneScan -s [seq_file_name] -o [output_file_name] -w [1 or 0] -t [train_file_name] (-p [thread_num])\n\n");
-  printf("%s", "       Mandatory parameters\n");
-  printf("%s", "       [seq_file_name]:    sequence file name including the full path\n");
-  printf("%s", "       [output_file_name]: output file name including the full path\n");
-  printf("%s", "       [1 or 0]:           1 if the sequence file has complete genomic sequences\n");
-  printf("%s", "                           0 if the sequence file has short sequence reads\n");
-  printf("%s", "       [train_file_name]:  file name that contains model parameters; this file should be in the \"train\" directory\n");
-  printf("%s", "                           Note that four files containing model parameters already exist in the \"train\" directory\n");
-  printf("%s", "                           [complete] for complete genomic sequences or short sequence reads without sequencing error\n");
-  printf("%s", "                           [sanger_5] for Sanger sequencing reads with about 0.5% error rate\n");
-  printf("%s", "                           [sanger_10] for Sanger sequencing reads with about 1% error rate\n");
-  printf("%s", "                           [454_5] for 454 pyrosequencing reads with about 0.5% error rate\n");
-  printf("%s", "                           [454_10] for 454 pyrosequencing reads with about 1% error rate\n");
-  printf("%s", "                           [454_30] for 454 pyrosequencing reads with about 3% error rate\n");
-  printf("%s", "                           [illumina_5] for Illumina sequencing reads with about 0.5% error rate\n");
-  printf("%s", "                           [illumina_10] for Illumina sequencing reads with about 1% error rate\n\n");
-  printf("%s", "       Optional parameter\n");
-  printf("%s", "       [thread_num]:       the number of threads used by FragGeneScan; default is 1 thread.\n");
+    printf("%s", "USAGE: ./FragGeneScan -s [seq_file_name] -o [output_file_name] -w [1 or 0] -t [train_file_name] (-p [thread_num])\n\n");
+    printf("%s", "       Mandatory parameters\n");
+    printf("%s", "       [seq_file_name]:    sequence file name including the full path\n");
+    printf("%s", "       [output_file_name]: output file name including the full path\n");
+    printf("%s", "       [1 or 0]:           1 if the sequence file has complete genomic sequences\n");
+    printf("%s", "                           0 if the sequence file has short sequence reads\n");
+    printf("%s", "       [train_file_name]:  file name that contains model parameters; this file should be in the \"train\" directory\n");
+    printf("%s", "                           Note that four files containing model parameters already exist in the \"train\" directory\n");
+    printf("%s", "                           [complete] for complete genomic sequences or short sequence reads without sequencing error\n");
+    printf("%s", "                           [sanger_5] for Sanger sequencing reads with about 0.5% error rate\n");
+    printf("%s", "                           [sanger_10] for Sanger sequencing reads with about 1% error rate\n");
+    printf("%s", "                           [454_5] for 454 pyrosequencing reads with about 0.5% error rate\n");
+    printf("%s", "                           [454_10] for 454 pyrosequencing reads with about 1% error rate\n");
+    printf("%s", "                           [454_30] for 454 pyrosequencing reads with about 3% error rate\n");
+    printf("%s", "                           [illumina_5] for Illumina sequencing reads with about 0.5% error rate\n");
+    printf("%s", "                           [illumina_10] for Illumina sequencing reads with about 1% error rate\n\n");
+    printf("%s", "       Optional parameter\n");
+    printf("%s", "       [thread_num]:       the number of threads used by FragGeneScan; default is 1 thread.\n");
 }
 
 /**
 * Custom error function to print allocation errors.
 * Mostly called from matrix or vector functions.
 */
-void print_allocation_error(const char * format,...){
+void print_allocation_error(const char * format,...) {
     va_list list;
     va_start(list,format);
     vfprintf(stderr, format,list);
